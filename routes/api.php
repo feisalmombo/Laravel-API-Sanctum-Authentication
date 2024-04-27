26ad9d7326da979d20cf530f97671bea21037ad1
feisalmombo <feisalmombo29@gmail.com>
2024-04-27 13:38:55 +0300

    WIP

--- routes/api.php
+++ routes/api.php
@@ -16,12 +16,8 @@ use App\Http\Controllers\AuthLoginRegisterController;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
 
-
-// Public routes of authtication
+// Public routes of authentication
 Route::controller(AuthLoginRegisterController::class)->group(function() {
     Route::post('/register', 'register');
     Route::post('/login', 'login');
